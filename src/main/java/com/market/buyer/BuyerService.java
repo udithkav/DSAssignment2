2debba6dd90a299716e53ed7109b07d307388a76
IT19085968 <it19085968@my.sliit.lk>
2021-05-20 10:06:10 +0530

    Update function done

--- src/main/java/com/market/buyer/BuyerService.java
+++ src/main/java/com/market/buyer/BuyerService.java
@@ -5,7 +5,9 @@ package com.market.buyer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -39,5 +41,53 @@ public class BuyerService {
         }
 
         buyerRepository.deleteById(buyerId);
+    }
+
+    @Transactional
+    public void updateTheOrder(Long buyerId,
+                               String email,
+                               String firstname,
+                               String lastname,
+                               String address,
+                               String pnone) {
+        Buyer buyer = buyerRepository.findById(buyerId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "order of buyer with id" +buyerId+"does not exist"
+                ));
+
+        if (email != null
+                && email.length() > 0 &&
+                !Objects.equals(buyer.getEmail(), email)) {
+            Optional<Buyer> customerOptional = buyerRepository
+                    .findCustomerByEmail(email);
+            if (customerOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            buyer.setEmail(email);
+        }
+
+        if (firstname != null
+                && firstname.length() > 0 &&
+                !Objects.equals(buyer.getFirstname(), firstname)) {
+            buyer.setFirstname(firstname);
+        }
+
+        if (lastname != null
+                && lastname.length() > 0 &&
+                !Objects.equals(buyer.getLastname(), lastname)) {
+            buyer.setLastname(lastname);
+        }
+
+        if (address != null
+                && address.length() > 0 &&
+                !Objects.equals(buyer.getAddress(), address)) {
+            buyer.setAddress(address);
+        }
+
+        if (pnone != null
+                && pnone.length() > 0 &&
+                !Objects.equals(buyer.getPnone(), pnone)) {
+            buyer.setPnone(pnone);
+        }
     }
 }
